05c8a2ce3ce50d992189aced836e984acbfbee43
Abu-Sayed-Sarker <abusyed96728@gmail.com>
2024-11-13 13:36:04 +0600

    practic complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import './App.css'
+import { auth } from './firebase'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [user, setuser] = useState(null)
+
+  const provider = new GoogleAuthProvider()
+
+  const googleSinein = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        setuser(result.user)
+      })
+    .catch(erroor => console.log('error', erroor))
+  }
+  
+
+
+  const removeUser = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sing out done");
+        setuser(null)
+      
+      })
+    .catch(error => console.log("Error", error)
+    )
+  }
 
   return (
     <>
+      <h1>Semple Log in sestem</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        {
+          user ?
+            <div>
+              <div>
+                <h3>{user.displayName}</h3>
+                <p>Dear {user.displayName} your log in succesfull</p>
+              </div>
+              <button onClick={removeUser}> log Out</button>
+            </div>  
+           :
+            <button onClick={googleSinein}> log IN</button>
+          
+        }
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWPV48cCPBMgu97MlUlqfwy1SOytdlfhc",
+    authDomain: "semple-firebase.firebaseapp.com",
+    projectId: "semple-firebase",
+    storageBucket: "semple-firebase.firebasestorage.app",
+    messagingSenderId: "746363080426",
+    appId: "1:746363080426:web:10da9e23b3cc3a6347d193"
+  };
+  
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {auth}
